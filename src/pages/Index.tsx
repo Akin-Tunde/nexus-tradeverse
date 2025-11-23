7c5d0f27966dace060f137cc856905dc4cb8e787
Akin-Tunde <wang23hao@gmail.com>
2025-11-23 14:50:08 -0800

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,3 +1,4 @@
+// FILE: src/pages/Index.tsx (Updated)
 import { useState } from "react";
 import { Header, TradeMode } from "@/components/Header";
 import { Footer } from "@/components/Footer";
@@ -5,25 +6,23 @@ import { MarketsSidebar } from "@/components/trading/MarketsSidebar";
 import { TradingChart } from "@/components/trading/TradingChart";
 import { OrderPanel } from "@/components/trading/OrderPanel";
 import { PositionsTable } from "@/components/trading/PositionsTable";
+import { useTradingData } from "@/components/ui/use-toast"; // <-- NEW IMPORT
 
 const Index = () => {
-  const [tradeMode, setTradeMode] = useState<TradeMode>("futures");
-  const [selectedMarket, setSelectedMarket] = useState("BTCUSD");
+  // State is now managed in useTradingData hook
+  const { tradeMode, setTradeMode, selectedMarket, setSelectedMarket } = useTradingData(); 
 
   return (
     <div className="min-h-screen bg-background pb-20">
-      <Header tradeMode={tradeMode} onTradeModeChange={setTradeMode} />
+      <Header /> {/* Removed props */}
       
       <div className="flex h-[calc(100vh-3.5rem-5rem)]">
-        <MarketsSidebar 
-          selectedMarket={selectedMarket}
-          onSelectMarket={setSelectedMarket}
-        />
+        <MarketsSidebar /> {/* Removed props */}
         
         <div className="flex-1 flex flex-col">
           <div className="flex-1 flex gap-4 p-4">
             <TradingChart market={selectedMarket} />
-            <OrderPanel market={selectedMarket} />
+            <OrderPanel /> {/* Removed props */}
           </div>
           
           <div className="h-64 px-4 pb-4">
